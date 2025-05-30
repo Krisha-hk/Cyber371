f30de435f3e6033ce2d9b88088f03fa9a6377ca1
Krisha-hk <151496087+Krisha-hk@users.noreply.github.com>
2025-05-30 20:25:28 +1200

    Update SNMP.py

--- SNMP.py
+++ SNMP.py
@@ -1,34 +1,20 @@
-from pysnmp.hlapi import (
-    SnmpEngine,
-    CommunityData,
-    UdpTransportTarget,
-    ContextData,
-    ObjectType,
-    ObjectIdentity,
-    getCmd
+from scapy.all import *
+from scapy.layers.snmp import SNMP, SNMPget, SNMPvarbind
+
+SNMP_SERVER = "192.168.56.4"  # Change to your SNMP server IP
+COMMUNITY = "public"
+OID = "1.3.6.1.2.1.1.1.0"     # sysDescr (System Description)
+
+# Create the SNMP GET request packet
+snmp_request = IP(dst=SNMP_SERVER)/UDP(sport=RandShort(), dport=161)/SNMP(
+    community=COMMUNITY,
+    PDU=SNMPget(varbindlist=[SNMPvarbind(oid=OID)])
 )
 
-SNMP_SERVER = "192.168.56.4"
-COMMUNITY = "public"
+print(f"Sending SNMP GET to {SNMP_SERVER} for OID {OID}...")
+response = sr1(snmp_request, timeout=5, verbose=0)
 
-def query_snmp():
-    errorIndication, errorStatus, errorIndex, varBinds = next(
-        getCmd(
-            SnmpEngine(),
-            CommunityData(COMMUNITY, mpModel=1),  # SNMPv2c
-            UdpTransportTarget((SNMP_SERVER, 161), timeout=1, retries=1),
-            ContextData(),
-            ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
-        )
-    )
-
-    if errorIndication:
-        print(f"Error: {errorIndication}")
-    elif errorStatus:
-        print(f"Error: {errorStatus.prettyPrint()}")
-    else:
-        for varBind in varBinds:
-            print(f"{varBind[0]} = {varBind[1]}")
-
-if __name__ == "__main__":
-    query_snmp()
+if response:
+    response.show()
+else:
+    print("No response from SNMP server.")
